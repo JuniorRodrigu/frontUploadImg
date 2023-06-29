86c5ce68d04bddcd082aa3654dd0f26dc35b22b8
JUNIOR RODRIGUES <juniorrodrigues1717@gmail.com>
2023-06-29 07:28:12 -0300

    Atualizar o configApi.js

--- src/config/configApi.js
+++ src/config/configApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://back-upload-img.vercel.app"
+    baseURL: "VUw7oclg9sST@ep-muddy-dew-129795.us-east-2.aws.neon.tech"
 });
